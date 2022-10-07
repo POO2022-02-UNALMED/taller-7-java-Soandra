3859ed2bd22c8abf895611b3eecc3270ba710c74
soandadep <>
2022-10-06 22:06:46 -0500

    last fix

--- src/main/java/comunicacion/Alfabeto.java
+++ src/main/java/comunicacion/Alfabeto.java
@@ -21,7 +21,7 @@ public class Alfabeto extends Pictograma{
 
     @Override
     public String toString() {
-        return "a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z";
+        return "A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z";
     }
 
     public String[] getLetras() {
